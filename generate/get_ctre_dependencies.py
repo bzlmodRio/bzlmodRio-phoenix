2c41bf6cacea41ddf01459b55355958d03b52b08
PJ Reiniger <pj.reiniger@gmail.com>
2023-05-05 22:19:37 -0400

    Gen stuff

--- generate/get_ctre_dependencies.py
+++ generate/get_ctre_dependencies.py
@@ -10,7 +10,7 @@ from bazelrio_gentool.deps.dependency_container import (
 def get_ctre_dependencies(
     use_local_allwpilib=False, use_local_opencv=False, use_local_ni=True
 ):
-    SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+    SCRIPT_DIR = os.environ["BUILD_WORKSPACE_DIRECTORY"]
     group = vendordep_dependency(
         "bzlmodrio-phoenix",
         os.path.join(SCRIPT_DIR, f"vendor_dep.json"),
