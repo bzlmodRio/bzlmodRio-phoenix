d5104a17007af28bb8385ce55c45b99c4854c4a5
PJ Reiniger <pj.reiniger@gmail.com>
2024-10-04 03:21:39 -0400

    Again

--- generate/get_phoenix_dependencies.py
+++ generate/get_phoenix_dependencies.py
@@ -15,8 +15,8 @@ def get_phoenix_dependencies(
     use_local_ni=True,
     allwpilib_version_override="2024.1.1",
     phoenix6_version_override="24.0.0-beta-7",
-    opencv_version_override="2024.4.8.0-1",
-    ni_version_override="2024.2.1",
+    opencv_version_override="2024.4.8.0-4.bcr1",
+    ni_version_override="2024.2.1.bcr1",
 ):
     sim_install_name_classes = [
         "simCANCoder",
