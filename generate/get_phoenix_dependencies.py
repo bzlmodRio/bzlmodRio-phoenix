700cf8643f78225c4741269873727b8a956728d6
PJ Reiniger <pj.reiniger@gmail.com>
2024-12-27 17:30:56 -0500

    Update wpilib version

--- generate/get_phoenix_dependencies.py
+++ generate/get_phoenix_dependencies.py
@@ -11,9 +11,9 @@ def get_phoenix_dependencies(
     use_local_phoenix6=False,
     use_local_opencv=False,
     use_local_ni=True,
-    allwpilib_version_override="2025.1.1-beta-1",
+    allwpilib_version_override="2025.1.1-beta-3",
     phoenix6_version_override="25.0.0-beta-2",
-    opencv_version_override="2024.4.8.0-4.bcr1",
+    opencv_version_override="2025.4.10.0-2",
     ni_version_override="2025.0.0",
 ):
     sim_install_name_classes = [
