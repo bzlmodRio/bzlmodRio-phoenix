51e66cf18ebc8a451e3f71dc4b0c30dce1f111bc
PJ Reiniger <pj.reiniger@gmail.com>
2025-05-13 03:42:08 -0400

    Lint

--- generate/get_phoenix_dependencies.py
+++ generate/get_phoenix_dependencies.py
@@ -11,7 +11,7 @@ def get_phoenix_dependencies(
     use_local_phoenix6=False,
     use_local_opencv=False,
     use_local_ni=True,
-   allwpilib_version_override="2025.3.2.bcr1",
+    allwpilib_version_override="2025.3.2.bcr1",
     phoenix6_version_override="25.1.0",
     opencv_version_override="2025.4.10.0-3.bcr2",
     ni_version_override="2025.2.0.bcr1",
